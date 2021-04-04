3e0c2da2345fea7b5cd09b963b8d5050c36b0d5d
m6xcom <65496114+m6xcom@users.noreply.github.com>
2021-04-04 19:48:06 +0300

    Adds code of app

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,31 @@
-import { combineReducers } from "redux";
-import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { contacts } from "./contacts/contacts-reducer";
 
-const rootReducer = combineReducers({
-  contacts,
-});
+const contactsConfig = {
+  key: "contacts",
+  storage,
+  blacklist: ["filter"],
+};
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: { contacts: persistReducer(contactsConfig, contacts) },
   devTools: process.env.NODE_ENV === "development",
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
+
+export const persistor = persistStore(store);
