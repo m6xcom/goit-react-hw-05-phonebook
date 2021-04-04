847110089ef01a200a5111cc76c8f24bdb4095ac
m6xcom <65496114+m6xcom@users.noreply.github.com>
2021-04-04 19:49:33 +0300

    Adds code of app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,35 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import ContactForm from "../ContactForm/ContactForm";
 import Filter from "../Filter/Filter";
 import ContactsList from "../ContactsList/ContactsList";
 import "./App.css";
 
-class App extends Component {
-  // componentDidMount() {
-  //   console.log(this.state.items);
-  //   const storageContacts = JSON.parse(localStorage.getItem("contacts"));
-  //   if (storageContacts) {
-  //     this.setState({
-  //       contacts: storageContacts,
-  //     });
-  //   }
-  // }
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (this.state.contacts !== prevState.contacts) {
-  //     localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
-  //   }
-  // }
-  render() {
-    return (
-      <div className="wrapper">
-        <h2>Phonebook</h2>
-        <ContactForm />
-        <h2 className="contactsTitle">Contacts</h2>
-        <Filter />
-        <ContactsList />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="wrapper">
+      <h2>Phonebook</h2>
+      <ContactForm />
+      <h2 className="contactsTitle">Contacts</h2>
+      <Filter />
+      <ContactsList />
+    </div>
+  );
+};
 
 export default App;
